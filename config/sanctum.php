b9e4c7a9e61355426ecb969219eba6d34f6cf066
sajan <sajansajan85611@gmail.com>
2026-03-11 10:27:00 +0530

    bug fix

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,karmafrontend.vercel.app')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
     'guard' => ['web'],
     'expiration' => null,
     'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
