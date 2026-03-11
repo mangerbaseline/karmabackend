49e8adec203cae2772215a7b604886a8cd0ff7b7
mangerbaseline <manageratbaseline@gmail.com>
2026-03-11 09:35:54 +0530

    Add allowed origin for Karma frontend

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://karmafrontend.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => [
